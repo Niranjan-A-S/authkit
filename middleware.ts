31afc1a0a6fe7a7ec8586abe03799fa0e51cdb1c
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-07 20:31:41 +0530

    chore: middleware setup

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,34 @@
-import NextAuth from 'next-auth';
 import authConfig from '@/auth.config';
+import NextAuth from 'next-auth';
+import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from './routes';
 
 const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
+export default auth(((req: any) => {
+    const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
-});
+
+    const isAPIAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if (isAPIAuthRoute) {
+        return null;
+    }
+
+    if (isAuthRoute) {
+        if (isLoggedIn) {
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+        }
+        return null;
+    }
+
+    if (!isPublicRoute && !isLoggedIn) {
+        return Response.redirect(new URL('/auth/login', nextUrl));
+    }
+
+    return null;
+}) as any);
 
 export const config = {
     matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
