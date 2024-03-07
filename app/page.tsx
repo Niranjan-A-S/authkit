3e1ac43cf0a173753068786e9f4808ade337c2eb
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-07 13:27:20 +0530

    chore: fixed eslint errors and warnings

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { cn } from "@/lib/utils";
-import { Poppins } from "next/font/google";
+import { cn } from '@/lib/utils';
+import { Poppins } from 'next/font/google';
 
 const font = Poppins({
-  subsets: ["latin"],
-  weight: ["600"]
-})
+  subsets: ['latin'],
+  weight: ['600']
+});
 
 export default function Home() {
   return (
