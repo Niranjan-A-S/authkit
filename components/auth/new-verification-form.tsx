bfe3a78fde33a67d2cc00276b80bfca273347121
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-18 00:34:53 +0530

    feat: completed the email verification functionality

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -2,12 +2,14 @@
 
 import { IFormResponse } from '@/types/component-props';
 import { useSearchParams } from 'next/navigation';
-import { FC, memo, useCallback, useEffect, useState } from 'react';
+import { FC, memo, useCallback, useEffect, useMemo, useState } from 'react';
 import { FormResponse } from '../form-response';
 import { CardWrapper } from './card-wrapper';
+import { verifyEmail } from '@/actions/new-verification';
+import { BeatLoader } from 'react-spinners';
 
 const defaultResponse: IFormResponse = {
-    type: 'error',
+    type: 'success',
     message: ''
 };
 
@@ -16,32 +18,36 @@ export const NewVerificationForm: FC = memo(() => {
     const [response, setResponse] = useState<IFormResponse>(defaultResponse);
     const searchParams = useSearchParams();
 
-    const token = searchParams.get('token');
+    const token = useMemo(() => searchParams.get('token'), [searchParams]);
 
-    const onSubmit = useCallback(async (token: string) => {
-        //TODO: new verification action
-    }, []);
-
-    useEffect(() => {
+    const onSubmit = useCallback(async () => {
         if (response.message) return;
         if (!token) return setResponse({ type: 'error', message: 'Verification token is missing' });
 
-        onSubmit(token);
+        const { error, success } = await verifyEmail(token);
+
+        if (error) setResponse({ type: 'error', message: error });
+        else setResponse({ type: 'success', message: success });
+    }, [response.message, token]);
+
+    useEffect(() => {
+        onSubmit();
     }, [onSubmit, response.message, token]);
 
-    return <CardWrapper
-        headerLabel="Confirming your verification"
-        backButtonLabel="Back to login"
-        backButtonHref="/auth/login"
-    >
-        <div className="flex items-center w-full justify-center">
-            {
-                response.message
-                    ? <FormResponse response={response} />
-                    : <span>Loading</span>
-            }
-            {/* TODO:use BeatSpinner from react-spinners */}
-        </div>
+    return (
+        <CardWrapper
+            headerLabel="Confirming your verification"
+            backButtonLabel="Back to login"
+            backButtonHref="/auth/login"
+        >
+            <div className="flex items-center w-full justify-center">
+                {
+                    response.message
+                        ? <FormResponse response={response} />
+                        : <BeatLoader />
+                }
+            </div>
 
-    </CardWrapper>;
+        </CardWrapper>
+    );
 });
