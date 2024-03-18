44e4ca9d816b890268912689d0ca5537a856ff5a
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-18 21:17:03 +0530

    feat: completed the 2FA in login action

--- actions/login.ts
+++ actions/login.ts
@@ -1,9 +1,13 @@
+/* eslint-disable complexity */
 'use server';
 
 import { signIn } from '@/auth';
+import { db } from '@/lib/db';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { LoginSchema } from '@/schemas';
-import { sendVerificationEmail } from '@/utils/mail';
+import { sendTwoFactorToken, sendVerificationEmail } from '@/utils/mail';
+import { getTwoFactorConfirmationFromUserId } from '@/utils/two-factor-confirmation';
+import { generateTwoFactorToken, getTwoFactorTokenFromEmail } from '@/utils/two-factor-token';
 import { getUserByEmail } from '@/utils/user';
 import { generateVerificationToken } from '@/utils/verification-token';
 import { AuthError } from 'next-auth';
@@ -14,7 +18,7 @@ export const login = async (value: z.infer<typeof LoginSchema>) => {
     if (!validatedFields.success) return {
         error: 'Invalid email or password'
     };
-    const { email, password } = validatedFields.data;
+    const { email, password, code } = validatedFields.data;
 
     const existingUser = await getUserByEmail(email);
     if (!existingUser || !existingUser.email || !existingUser.password) return { error: 'User with given email does  not exist!' };
@@ -28,6 +32,52 @@ export const login = async (value: z.infer<typeof LoginSchema>) => {
         return { success: 'Confirmation email sent.' };
     }
 
+    if (existingUser.isTwoFactorEnabled && existingUser.email) {
+        if (code) {
+            const twoFactorToken = await getTwoFactorTokenFromEmail(email);
+            if (!twoFactorToken) return {
+                error: 'Invalid 2FA code'
+            };
+
+            if (twoFactorToken.token !== code) return {
+                error: 'The 2FA code you have entered is wrong!'
+            };
+
+            const hasExpired = new Date(twoFactorToken.expires) < new Date();
+            if (hasExpired) return {
+                error: 'The 2FA code has expired!'
+            };
+
+            await db.twoFactorToken.delete({
+                where: {
+                    id: twoFactorToken.id
+                }
+            });
+
+            const existingConfirmation = await getTwoFactorConfirmationFromUserId(existingUser.id);
+
+            if (existingConfirmation) await db.twoFactorConfirmation.delete({
+                where: {
+                    id: existingConfirmation.id
+                }
+            });
+
+            await db.twoFactorConfirmation.create({
+                data: {
+                    userId: existingUser.id
+                }
+            });
+        }
+        else {
+            const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+
+            const { error } = await sendTwoFactorToken(twoFactorToken.email, twoFactorToken.token);
+            if (error) return { error: 'Failed to send 2FA code' };
+
+            return { twoFactor: 'Two Factor Authentication code sent!' };
+        }
+    }
+
     try {
         await signIn('credentials', {
             email,
@@ -38,11 +88,11 @@ export const login = async (value: z.infer<typeof LoginSchema>) => {
         if (error instanceof AuthError) {
             switch (error.type) {
                 case 'CredentialsSignin':
-                    return { error: 'Invalid credentials' };
+                    return { error: 'Invalid credentials!' };
                 case 'AccessDenied':
-                    return { error: 'Access Denied' };
+                    return { error: 'Access Denied!' };
                 default:
-                    return { error: 'Something went wrong' };
+                    return { error: 'Something went wrong!' };
             }
         }
         //TODO what is the issue??
