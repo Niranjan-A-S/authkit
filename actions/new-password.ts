8b3596b10bab5110295ef2aec15e8719864ab649
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-18 16:52:59 +0530

    chore: completed the password update action

---
+++ actions/new-password.ts
@@ -0,0 +1,53 @@
+'use server';
+
+import { db } from '@/lib/db';
+import { NewPasswordSchema } from '@/schemas';
+import { generateHashedPassword } from '@/utils/hash';
+import { deletePasswordResetToken, getPasswordResetTokenFromToken } from '@/utils/password-reset-token';
+import { getUserByEmail } from '@/utils/user';
+import z from 'zod';
+
+export const newPassword = async (
+    values: z.infer<typeof NewPasswordSchema>,
+    token?: string | null
+) => {
+    if (!token) return { error: 'Password reset token is missing!' };
+
+    const validatedFields = NewPasswordSchema.safeParse(values);
+
+    if (!validatedFields.success) return {
+        error: 'Password does not meet the required constraints!'
+    };
+
+    const { password } = validatedFields.data;
+
+    const existingToken = await getPasswordResetTokenFromToken(token);
+    if (!existingToken) return {
+        error: 'Invalid password reset token!'
+    };
+
+    const hasExpired = new Date(existingToken.expires) < new Date();
+    if (hasExpired) return {
+        error: 'Password reset token has expired!'
+    };
+
+    const existingUser = await getUserByEmail(existingToken.email);
+    if (!existingUser) return {
+        error: 'Email not found for the given token!'
+    };
+
+    const hashedPassword = await generateHashedPassword(password);
+
+    await db.user.update({
+        where: {
+            id: existingUser.id
+        },
+        data: {
+            password: hashedPassword
+        }
+    });
+
+    await deletePasswordResetToken(existingToken.id);
+
+    return { success: 'Password updated!' };
+};
