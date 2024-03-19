8921306d71772ec5ed9f3fd3aaa7021c9edcb8c1
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-19 20:15:37 +0530

    chore: created test API route for admin

---
+++ app/api/admin/route.ts
@@ -0,0 +1,12 @@
+import { getCurrentUser } from '@/utils/auth';
+import { Role } from '@prisma/client';
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+    const role = (await getCurrentUser())?.role;
+
+    if (role === Role.ADMIN) return new NextResponse(null, { status: 200 });
+
+    return new NextResponse(null, { status: 403 });
+};
+
