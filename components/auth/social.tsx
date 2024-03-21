93226b35d9c04a7176a2be5d8492fd2c716ab434
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-21 17:37:35 +0530

    chore: added callback url while using providers login

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -3,15 +3,19 @@
 import { Button } from '@/components/ui/button';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { signIn } from 'next-auth/react';
-import { FC, memo, useCallback } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { FC, memo, useCallback, useMemo } from 'react';
 import { FaGithub } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
 
 export const Social: FC = memo(() => {
 
+    const searchParams = useSearchParams();
+    const callbackUrl = useMemo(() => searchParams.get('callbackUrl'), [searchParams]);
+
     const onClick = useCallback((provider: 'google' | 'github') => {
-        signIn(provider, { callbackUrl: DEFAULT_LOGIN_REDIRECT });
-    }, []);
+        signIn(provider, { callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT });
+    }, [callbackUrl]);
 
     return (
     <div className="flex items-center w-full gap-x-2">
