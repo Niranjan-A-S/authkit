fd3a5cc85da3b042136270b10850e97183f6bb52
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-21 17:36:20 +0530

    chore: login form dialog

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -3,6 +3,9 @@
 import { ILoginButtonProps } from '@/types/component-props';
 import { useRouter } from 'next/navigation';
 import React, { FC, memo, useCallback } from 'react';
+import { Dialog, DialogContent } from '../ui/dialog';
+import { DialogTrigger } from '@radix-ui/react-dialog';
+import { LoginForm } from './login-form';
 
 export const LoginButton: FC<ILoginButtonProps> = memo(({ children, asChild, mode = 'redirect' }) => {
 
@@ -14,9 +17,14 @@ export const LoginButton: FC<ILoginButtonProps> = memo(({ children, asChild, mod
 
 	return mode === 'modal'
 		? (
-			<span>
-				TODO: Implement modal
-			</span>
+			<Dialog>
+				<DialogTrigger asChild={asChild}>
+					{children}
+				</DialogTrigger>
+				<DialogContent className='p-0 w-auto bg-transparent border-none'>
+					<LoginForm />
+				</DialogContent>
+			</Dialog>
 		)
 		: (
 			<span onClick={onClick} className="cursor-pointer">
