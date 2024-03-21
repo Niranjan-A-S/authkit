c845e2659c2385e8b2ce24d7d5489f95343e83b1
Niranjan Avanippilli Santhosh <niranjan.santhosh@agilepoint.com>
2024-03-21 17:35:14 +0530

    chore: added callback url in the redirect

--- middleware.ts
+++ middleware.ts
@@ -24,7 +24,14 @@ export default auth(((req: any) => {
     }
 
     if (!isPublicRoute && !isLoggedIn) {
-        return Response.redirect(new URL('/auth/login', nextUrl));
+
+        let callbackUrl = nextUrl.pathname;
+        if (nextUrl.search) {
+            callbackUrl += nextUrl.search;
+        }
+
+        const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+        return Response.redirect(new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl));
     }
 
     return null;
